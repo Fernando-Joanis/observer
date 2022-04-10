90f7fef800e8350fa8cc3f2ec7bae348a694e7c2
Fernando Joanis <fjoanis@gmail.com>
2022-04-10 20:46:59 -0300

    Criando commit inicial

---
+++ main.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+class IObservable(ABC):
+
+    @property
+    @abstractmethod
+    def state(self): pass
+
+    @abstractmethod
+    def add_observer(self, observer: IObserver) -> None:
+        pass
+
+    @abstractmethod
+    def remove_observer(self, observer: IObserver) -> None:
+        pass
+
+    @abstractmethod
+    def notify_observers(self) -> None:
+        pass
+
+class Socket(IObservable):
+    def __init__(self):
+        self._observers: List[IObserver] = []
+        self._state: Dict = {}
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, state_update: Dict):
+        new_state: Dict = {**self._state, **state_update}
+
+        if new_state != self._state:
+            self._state = new_state
+            self.notify_observers()
+
+    def reset_state(self):
+        self._state = {}
+
+    def add_observer(self, observer: IObserver) -> None:
+        self._observers.append(observer)
+
+    def remove_observer(self, observer: IObserver) -> None:
+        if observer in self._observers:
+            self._observers.remove(observer)
+
+    def notify_observers(self) -> None:
+        for observer in self._observers:
+            observer.update(self._state)
+
+
---
+++ observer.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from main import Socket, IObservable
+class IObserver(ABC):
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+class Upload(IObserver):
+    def __init__(self, nome, observable: IObservable):
+        self.nome = nome
+        self.observable = observable
+
+    def update(self, state):
+        if state['connect'] == True:
+            print('Iniciar upload')
+        elif  state['connect'] == False:
+            print('Parar Upload')
+
+if __name__ == '__main__':
+    sio = Socket()
+    upload = Upload('Upload', sio)
+
+    sio.add_observer(upload)
+
+    sio.state = {'connect': True}
+    sio.state = {'connect': True}
+    sio.state = {'connect': False}
